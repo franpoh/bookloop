cba4fe607d9a84f67877efda5f5286de4d526630
AaronC <91234889+AaronCWH@users.noreply.github.com>
2022-03-01 17:47:23 +0800

    updated text area, added genres, not linked with database yet

--- web-app/src/screens/upload-book/index.js
+++ web-app/src/screens/upload-book/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 // import API from "../API";
 import "./UploadBook.css";
 
@@ -13,7 +14,9 @@ function UploadBook() {
                 <div className='leftContainer'>
 
                     <div className='inputContainer'>
+
                         <h2>What book would you like to upload today?</h2>
+                        
                         <form>
 
                             <label for='booktitle'> Book Title: </label><br/>
@@ -22,6 +25,20 @@ function UploadBook() {
                             <label for='bookauthor'> Author: </label><br/>
                             <input type='text' id='bookauthor' name='bookauthor'/><br/><br/>
 
+                            <label for='bookgenre'> Genre: </label><br/>
+                            <select name='bookgenre' id='bookgenre'>
+                                <option value="1">Horror</option>
+                                <option value="2">Thriller</option>
+                                <option value="3">Mystery</option>
+                                <option value="4">Sci-fi</option>
+                                <option value="5">Romance</option>
+                                <option value="6">Dystopian</option>
+                                <option value="7">Self-help</option>
+                                <option value="8">Biblography</option>
+                                <option value="9">Non-Fiction</option>
+                                <option value="10">Fiction</option>
+                            </select><br/><br/>
+
                             <label for='bookyear'> Year of Publishing: </label><br/>
                             <input type='text' id='bookyear' name='bookyear'/><br/><br/>
 
@@ -29,12 +46,13 @@ function UploadBook() {
                             <input type='text' id='bookyear' name='bookyear'/><br/><br/>
                             
                             <label for='bookreview'> Book Review (Optional): </label><br/>
-                            <textarea id='bookreview' rows='10' cols='60'>Occaecat nulla deserunt exercitation adipisicing reprehenderit veniam excepteur laborum duis eiusmod elit reprehenderit elit. Pariatur quis consequat qui est occaecat ut enim sit. Laboris enim commodo excepteur excepteur.
+                            <textarea>Occaecat nulla deserunt exercitation adipisicing reprehenderit veniam excepteur laborum duis eiusmod elit reprehenderit elit. Pariatur quis consequat qui est occaecat ut enim sit. Laboris enim commodo excepteur excepteur.
                             Est deserunt laboris voluptate duis quis amet eu nisi nostrud proident laborum fugiat occaecat. Voluptate labore qui do dolore. Cillum sunt commodo eiusmod sit adipisicing non. Irure sint dolore in ex labore. Commodo in fugiat et eu irure anim eu nisi adipisicing sint consequat.</textarea><br/><br/>
                             
                             <input type='submit' value="Upload to Bookswap"/>
 
                         </form>
+
                     </div>
 
                 </div>
