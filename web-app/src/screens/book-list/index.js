f6d1e1ec199738c72fb7ced0fbb417d1cbc70528
AuntPyoneMaung <za7196@gmail.com>
2022-03-01 23:16:01 +0800

    text input field styling added

--- web-app/src/screens/book-list/index.js
+++ web-app/src/screens/book-list/index.js
@@ -1,6 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import "../../styling/styles.css";
+import TextField from "@mui/material/TextField";
+import bookAPI from "../../API/book-api";
+
 function BookList() {
-    return(
-        <h1>BookList</h1>
+
+    const [searchText, setSearchText] = useState("");
+    const [data, setData] = useState([]);
+
+
+    return (
+        <div>
+            <h1>BookList</h1>
+            <div className="search">
+                <div>
+                    <TextField
+                        id="outlined-basic"
+                        variant="outlined"
+                        fullWidth
+                        label="Search Books"
+                    />
+                </div>
+
+            </div>
+        </div>
+
     )
 }
 
