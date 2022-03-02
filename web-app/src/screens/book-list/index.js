8a735dedc9ad218ba04e50012ecdfb887c73a357
AuntPyoneMaung <za7196@gmail.com>
2022-03-03 01:31:01 +0800

    retrieved titles from API

--- web-app/src/screens/book-list/index.js
+++ web-app/src/screens/book-list/index.js
@@ -1,31 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import "../../styling/styles.css";
+import styles from "../../styling/style-sheet"
 import TextField from "@mui/material/TextField";
 import bookAPI from "../../API/book-api";
+import Title from "./title"
 
-function BookList() {
+const BookList = () => {
 
-    const [searchText, setSearchText] = useState("");
-    const [data, setData] = useState([]);
+    const [book, setBook] = useState({
+        title: "",
+        image: ''
+    });
+    const [title, setTitle] = useState([]);
 
+    useEffect(() => {
+        bookAPI.get(`general/search?title=%20`)
+            .then(res => {
+                console.log("recieves these..:", res.data)
+                setTitle(res.data.data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [])
 
+    console.log("HEllo", title)
     return (
-        <div>
-            <h1>BookList</h1>
-            <div className="search">
-                <div>
-                    <TextField
-                        id="outlined-basic"
-                        variant="outlined"
-                        fullWidth
-                        label="Search Books"
-                    />
+
+        <>
+            <div>
+                <h1>BookList</h1>
+                <div style={{ ...styles.container }}>
+                    <div>
+                        <TextField
+                            id="outlined-basic"
+                            variant="outlined"
+                            fullWidth
+                            label="Search Books"
+                        />
+                    </div>
+                    {/* <img src={image} /> */}
+                    <Title data={title} />
                 </div>
 
             </div>
-        </div>
-
+        </>
     )
 }
+
 
 export default BookList;
