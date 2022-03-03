262e43feff22899c7043f8587d948f844ca1455b
AuntPyoneMaung <za7196@gmail.com>
2022-03-04 00:16:15 +0800

    refined search function, GET not dynamic

--- web-app/src/screens/book-list/index.js
+++ web-app/src/screens/book-list/index.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import styles from "../../styling/style-sheet"
-// import TextField from "@mui/material/TextField";
+import colours from "../../styling/colours";
+import TextField from "@mui/material/TextField";
 // import Autocomplete from '@mui/material/Autocomplete';
 import bookAPI from "../../API/book-api";
 // import Title from "./title";
+// import { useLoading, Audio } from '@agney/react-loading';
+
 
 const BookList = () => {
 
@@ -13,22 +16,19 @@ const BookList = () => {
     // });
     const [title, setTitle] = useState([]);
     const [searchInput, setSearchInput] = useState("");
+    const [params, setParams] = useState("%20")
 
-    // const retrieveTitles = async (titles) => {
-    //     const res = await bookAPI.get(`general/search?title=${titles.title}`, titles);
-    //     const {title} = res.data.data;
-    //     setTitle(
-    //         title.map(titles => {
-    //             return titles.
-    //         })
-    //     )
-    // }
+    const searchHandler = (event) => {
+        setSearchInput(event.target.value);
+        setParams(event.target.value)
+    }
 
     const retrieve = async () => {
         console.log("Fetching items from API Swap Index....");
-        await bookAPI.get(`general/search?title=%20`)
+        await bookAPI.get(`general/searchIndex?title=${params}`)
             .then(res => {
-                console.log("recieves these..:", res.data.data.Index);
+                console.log(searchInput)
+                console.log("recieves these..:", res.data.data);
                 setTitle(res.data.data);
             })
             .catch(err => {
@@ -38,42 +38,60 @@ const BookList = () => {
     }
 
     useEffect(() => {
+        // setParams(searchInput)
         retrieve();
+        console.log("useEffect")
     }, [])
 
-    console.log("title data...:", title)
+    // const { containerProps, indicatorEl } = useLoading({
+    //     loading: true,
+    //     indicator: <Audio width="50" />,
+    // });
+    // console.log("title data...:", title)
     return (
         <>
             <div>
                 <h1>BookList</h1>
                 <div style={{ ...styles.container }}>
 
-                    <input
+                    <TextField
                         style={{ ...styles.searcher }}
-                        type="text"
-                        placeholder="search for books..."
-                        onChange={event => {
-                            setSearchInput(event.target.value)
-                        }}
+                        id="outlined-basic"
+                        varient="outlined"
+                        fullWidth
+                        placeholder="search books"
+                        value={searchInput}
+                        onChange={searchHandler.bind(this)}
+
                     />
 
                     <div style={{ width: "45vw" }} >
-                        {title && title.filter(val => {
+                        {searchInput ? (title && title.filter(val => {
                             if (searchInput === "") {
                                 return val;
-                            } else if (val.Index.title.toLowerCase().includes(searchInput.toLowerCase())) {
+                            } else if (val.title.toLowerCase().includes(searchInput.toLowerCase())) {
                                 return val;
                             }
                         }).map((item, key) => {
                             return (
-                                <div key={key}>
-                                    <div>
-                                        <p>{item.Index.title}</p>
-                                        <img alt="covers" style={{ width: 100, height: 150 }} src={item.Index.imageURL} />
+                                <div style={{ background: `${colours.secondaryLight}` }} key={key}>
+                                    <div >
+                                        <p>{item.title}</p>
+                                        <img alt="covers" style={{ width: 100, height: 150 }} src={item.imageURL} />
                                     </div>
                                 </div>
                             )
-                        })}
+                        })) : (title.map((item, key) => {
+                            return (
+                                <div style={{ background: "lightgrey" }} key={key}>
+                                    <div>
+                                        <p>{item.title}</p>
+                                        <img alt="covers" style={{ width: 100, height: 150 }} src={item.imageURL} />
+                                    </div>
+                                </div>
+                            )
+                        }))
+                        }
                     </div>
                 </div>
 
