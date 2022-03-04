4006c05145426d34e4a0cc8b51bf0c36e7809489
CGW-G1 <91236669+CGW-G1@users.noreply.github.com>
2022-03-04 17:31:36 +0800

    Book details,  add/del wishlist

--- web-app/src/screens/book-details/index.js
+++ web-app/src/screens/book-details/index.js
@@ -1,45 +1,64 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import bookAPI from '../../API/book-api';
 
+import MyButton from "../../components/button";
 import '../../styling/colours.js';
-import '../../styling/styles.css';
 import styles from '../../styling/style-sheet';
+
+import removeBookfromWishList from './/remove-book-wishlist';
+import addBooktoWishList from './/add-book-wishlist';
 
 // icons?? images replacements? emojis?
 
 //mock place data
-import { userA, indexBooks, swapReviewMerged } from '../../test-data';
+import { swapReviewMerged } from '../../test-data';
 // let userA = {};
 // let swapReviewMerged = {};
-let userToken = false;
 
-function BookDetails( data ) {
-
-    const {state } = useLocation();
-    console.log('TESTING>>>>>>>>', state);
+function BookDetails() {
 
     const { index } = useParams();
 
-    console.log('useParams: ', index);
+    console.log('URL useParams: ', index);
 
     // need index from data
-    let indexId = 2;
-    
+    let indexId = parseInt(index);
     
     const [ matchIndex, updateMatchIndex] = useState({});
     const [ user, setUser ] = useState('')
+    const [ userToken, setUserToken] = useState(false); // for controlling display for non-login
+    const [userWishlist, updateUserWishlist] = useState([]);
+    const [currentBookWish, updateCurrentBookWish] = useState(false); // for toggling button status
 
+    // trigger on "component mount"
     useEffect(() => {
         retrieveBookDetails();
         retrieveUser();
-        retrieveAll();
-        if (user != '') {
-            userToken = true;            
-        };
-        console.log('token: ', userToken);
+        // retrieveAll();
     }, []);
+
+    // trigger on user update
+    useEffect(() => {        
+        if (user !== '') {
+            setUserToken(true);
+            updateUserWishlist(user.wishlist);
+        };
+    }, [user]);
+
+    useEffect(() => {
+        // check if already in wishlist
+        let xx;        
+        for (xx = 0; xx < userWishlist.length; xx++) {
+            if (userWishlist[xx] === indexId) {
+            console.log('book is already in wishlist');
+            updateCurrentBookWish(true);
+            } else {
+                console.log('book is NOT in wishlist', indexId, userWishlist[xx]);
+            };
+        };
+    }, [userWishlist]);
 
     // try catch for fetch book info
     async function retrieveBookDetails() {
@@ -49,19 +68,21 @@ function BookDetails( data ) {
             console.log(result.data);            
             console.log(result.data.data.title);
 
-            updateMatchIndex({...matchIndex, ...result.data.data});
+            updateMatchIndex(result.data.data);
+            // updateMatchIndex({...matchIndex, ...result.data.data});
         } catch (error) {
             console.log('Book Detail error: ', error);
         };
     };
 
-    ////////////////////////////////////////
     // try catch for user info if avail
     async function retrieveUser() {
         try {
             const result = await bookAPI.get('/protected/viewprofile');
 
             console.log('user: ', result.data.data);
+            if (result.data.data.user.wishlist === null) { result.data.data.user.wishlist = [] };
+            setUser(result.data.data.user); // Comment this out to test no login
 
         } catch (error) {
             console.log('User info error', error);
@@ -69,33 +90,21 @@ function BookDetails( data ) {
     };
 
     // G1 test
-    async function retrieveAll() {
-        try {
-            const result = await bookAPI.get('/protected/getusers');
+    // async function retrieveAll() {
+    //     try {
+    //         const result = await bookAPI.get('/protected/getusers');
 
-            console.log('All user: ', result.data.data);
+    //         console.log('All user: ', result.data.data);
 
-        } catch (error) {
-            console.log('User info error', error);
-        };
-    };
-    // let p = new Promise(async (resolve) => {
-    //     let result = await bookAPI.get('/protected/viewprofile');
-    //     let { data } = result;
-    //     let user = data.data;
-    //     resolve(user);
-    // })
+    //     } catch (error) {
+    //         console.log('User info error', error);
+    //     };
+    // };
     
-    // p.then((res) => {
-    //     setUser(res.username);
-    // });
-
-    // console.log("Test", user);  
-    ////////////////////////////////////////
-
 
     // need userid
-    const [localUser, updateLocalUser] = useState(userA);
+    
+    // const [localUser, updateLocalUser] = useState(userA);
 
     // need swap
     let localSwapBooks = swapReviewMerged;
@@ -103,40 +112,56 @@ function BookDetails( data ) {
     
     
 
-    // localUser.points = 0; // for testing when points unable to purchase books
+    // user.points = 0; // for testing when points unable to purchase books
 
-    if (localUser.wishlist === null) { localUser.wishlist = [] };
+    
 
     // const matchIndex = localIndexBooks.filter(data => data.indexId === indexId);
     // const matchSwap = localSwapBooks.filter(data => (data.indexId === indexId && data.availability === 'YES')); // check swap for YES/NO
 
     const [matchSwap, updateMatchSwap] = useState(localSwapBooks.filter(data => (data.indexId === indexId && data.availability === 'YES')));
 
-    const [userWishlist, updateUserWishlist] = useState(localUser.wishlist);
+    
 
-    // check if already in wishlist
-    let xx;
-    let wishYes = false;
-    for (xx = 0; xx < userWishlist.length; xx++) {
-        if (userWishlist[xx] === indexId) {
-        console.log('book is already in wishlist');
-        wishYes = true;
-        ;
-        };
-    };
+    
 
-    const [currentBookWish, updateCurrentBookWish] = useState(wishYes);
+    
 
 
     /////////
-    function WishButton() {
+    function wishButton() {
+
+        console.log("wishbutton, token: ", userToken);        
+
+        if (userToken === false){            
+            return;
+        } else if (currentBookWish) {
+            const updateWishlist = removeBookfromWishList({
+                indexId: indexId,
+                userWishlist: userWishlist
+              });
+            updateUserWishlist(updateWishlist);
+            updateCurrentBookWish(false);
+        } else if (!currentBookWish) {
+            const updateWishlist = addBooktoWishList({
+                indexId: indexId,
+                userWishlist: userWishlist
+              });            
+            updateUserWishlist(updateWishlist);
+            updateCurrentBookWish(true);
+        };
+
+
         return (
             <h2>wishbutton</h2>
         )
     };
 
     /////////
-    function UploadReviewButton() {
+    function uploadReviewButton() {
+
+        console.log("uploadReviewButton");
+
         return (
             <h2>UploadReviewButton</h2>
         )
@@ -150,34 +175,49 @@ function BookDetails( data ) {
         )
     };
 
-    if (matchIndex.indexId) {
-    console.log('image url', matchIndex.imageURL)} else console.log('index empty');
+    
     // matchIndex.imageURL = null; // for testing when book has no imageURL
 
     return(
-        <div style={styles.displayArea}>
-            <h1>{matchIndex.title}</h1>
+        <div style={styles.containerAlt}>
+            <h1 style={styles.h1Font}>{matchIndex.title}</h1>
             <div style={styles.displayRow}>
                 <div style={styles.displayCard}>
-                    {(matchIndex.imageURL != null) ? <img src={matchIndex.imageURL} style={{ height: 'auto', width:'100%' }} /> : <div ></div> }
+                    {(matchIndex.imageURL != null) ? <img src={matchIndex.imageURL} alt="Book Display" style={{...styles.profilePic, height: 'auto' }} /> : <div ></div> }
                 </div>                
                 <div style={{ marginLeft: 15 }}>
-                    {<h1 style={{...styles.textNormal, fontSize: '1em' }}>Author: {matchIndex.author}</h1>}
-                    {<h1 style={{...styles.textNormal, fontSize: '1em' }}>Year: {(matchIndex.year)?matchIndex.year:'-'}</h1>}
+                    {<h1 style={{...styles.textBold, fontSize: '1em' }}>Author: {matchIndex.author}</h1>}
+                    {<h1 style={{...styles.textBold, fontSize: '1em' }}>Year: {(matchIndex.year)?matchIndex.year:'-'}</h1>}
+                    {<h1 style={{...styles.textBold, fontSize: '1em' }}>Genre: {(matchIndex.genreId)?matchIndex.genreId:'-'}</h1>}
                 </div>
             </div>
             <hr style={styles.divider}/>
 
-            <div style={{ opacity: userToken ? 1 : 0.4 }}>
-                <h3 style={styles.textNormal}>Current available points: {(userToken) ? user.points : 'You are not logged in..' }</h3>
-                <WishButton />
-                <UploadReviewButton />    
+            <div style={{ position: 'relative',top: '-3vh', opacity: userToken ? 1 : 0.4 }}>
+                <h3 style={styles.textNormal}>Current available points: {(userToken) ? user.points : 'You are not logged in..' } {userToken? 'True':'False' }</h3>
+                <div style={styles.containerRow}>
+                <MyButton name={currentBookWish?"Now in Wishlist" : "Add to Wishlist"}
+                currentBookWish
+                    type={"button"}
+                    handle={
+                        () => wishButton()
+                    }
+                />
+                <MyButton name={"Upload Review"}
+                    type={"button"}
+                    handle={
+                        () => uploadReviewButton()
+                    }
+                />
+                </div>
+                {/* <WishButton /> */}
+                {/* <UploadReviewButton /> */}
             </div>
 
             
             
             {/* div style to check swap.length to show display as none or inline */}
-            <hr style={styles.divider}/>
+            <hr style={{...styles.divider, position: 'relative',top: '-3vh'}}/>
             <div>
                 <DisplaySwapInventory />
                 <DisplaySwapInventory />
