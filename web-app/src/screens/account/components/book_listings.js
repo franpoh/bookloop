d723ed9c645d76b1e3984f895eacebc7cae0b386
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-05 01:12:21 +0800

    testing out book navigation links

--- web-app/src/screens/account/components/book_listings.js
+++ web-app/src/screens/account/components/book_listings.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { v4 as uuidv4 } from 'uuid';
+import {  useNavigate } from 'react-router-dom';
 
 import styles from "../../../styling/style-sheet";
 
 const BookListing = (props) => {
+    const navigate = useNavigate();
+    let passIndex = 7; // test data
+
     const bookItem = props.item;
     const detailName = props.name;
     const detail = props.detail;
 
+    function navBook() {
+        navigate(`/bookdetails/${passIndex}`);
+    }
+
     return (
         <div key={uuidv4()} style={styles.containerRowList}>
-            <img key={uuidv4()} src={bookItem.Index.imageURL} alt="book covers" style={styles.profileBookPics} />
+            <img key={uuidv4()} src={bookItem.Index.imageURL} alt="book covers" style={styles.profileBookPics} onClick={navBook} />
             <div key={uuidv4()} style={styles.profileBookDetails} props>
-                <p key={uuidv4()} style={styles.textBold}>{bookItem.Index.title}</p>
-                <p key={uuidv4()} style={styles.textNormal}><b>{detailName}:</b> {bookItem[detail]}</p>
+                <p key={uuidv4()} style={styles.textBold} onClick={navBook}>{bookItem.Index.title}</p>
+                <p key={uuidv4()} style={styles.textNormal} onClick={navBook}><b>{detailName}:</b> {bookItem[detail]}</p>
             </div>
         </div>
     )
