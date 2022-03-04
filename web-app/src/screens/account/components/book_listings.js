f3d6407b5331ad8fc842422cb94ec633d6a04ed9
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-05 00:32:29 +0800

    added new props

--- web-app/src/screens/account/components/book_listings.js
+++ web-app/src/screens/account/components/book_listings.js
@@ -1,18 +1,19 @@
 import React from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 import styles from "../../../styling/style-sheet";
 
 const BookListing = (props) => {
-    const bookIndex = props.index;
     const bookItem = props.item;
+    const detailName = props.name;
     const detail = props.detail;
 
     return (
-        <div key={bookIndex} style={styles.containerRowList}>
-            <img src={bookItem.Index.imageURL} alt="book covers" style={styles.profileBookPics} />
-            <div style={styles.profileBookDetails} props>
-                <p style={styles.textBold}>{bookItem.Index.title}</p>
-                <p style={styles.textNormal}>"{bookItem[detail]}"</p>
+        <div key={uuidv4()} style={styles.containerRowList}>
+            <img key={uuidv4()} src={bookItem.Index.imageURL} alt="book covers" style={styles.profileBookPics} />
+            <div key={uuidv4()} style={styles.profileBookDetails} props>
+                <p key={uuidv4()} style={styles.textBold}>{bookItem.Index.title}</p>
+                <p key={uuidv4()} style={styles.textNormal}><b>{detailName}:</b> {bookItem[detail]}</p>
             </div>
         </div>
     )
@@ -21,7 +22,7 @@ const BookListing = (props) => {
 const BookDisplay = (props) => {
     return (
         <div style={styles.containerAlt}>
-            <h1 style={styles.h1Font}>Reviews</h1>
+            <h1 style={styles.h1Font}>{props.name}</h1>
             <ul style={styles.listNoBullets}>{props.uploaded}</ul>
         </div>
     )
