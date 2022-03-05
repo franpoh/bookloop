845ec3eaf3b6c3519241bca9db921661809c02cf
AuntPyoneMaung <za7196@gmail.com>
2022-03-05 18:48:45 +0800

    loading prorgess indicator, stylings update for BookList

--- web-app/src/screens/book-list/index.js
+++ web-app/src/screens/book-list/index.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from "../../styling/style-sheet"
 import "../../styling/style.css"
 import colours from "../../styling/colours";
-import TextField from "@mui/material/TextField";
-// import Autocomplete from '@mui/material/Autocomplete';
 import bookAPI from "../../API/book-api";
-// import Title from "./title";
-// import { useLoading, Audio } from '@agney/react-loading';
+
+import TextField from "@mui/material/TextField";
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import { useNavigate } from 'react-router-dom';
+import LinearProgress from '@mui/material/LinearProgress';
 
 
 const BookList = () => {
@@ -25,29 +25,28 @@ const BookList = () => {
     const retrieve = async () => {
         console.log("Fetching items from API....");
         try {
-            const data = await bookAPI.get(`general/searchIndex`)
+            await bookAPI.get(`general/searchIndex`)
                 .then(res => {
                     console.log("Fetching items from API....", res)
                     console.log("to read these..:", res.data.data);
                     setTitle(res.data.data);
-                    setIsLoading(false);
-                })
-        }
-        catch (err) {
+                });
+        } catch (err) {
             console.log("You have an error: ", err);
+        } finally {
+            setIsLoading(false);
         }
-    }
+    };
 
     useEffect(() => {
-        retrieve();
-        console.log("useEffect")
+        setTimeout(() => { // remove in finale
+            console.log("Delaying for testing");
+            retrieve();
+        }, 1000);
+        setIsLoading(true);
+        console.log("useEffect-ed")
     }, [])
 
-    // loading animation
-    // const { containerProps, indicatorEl } = useLoading({
-    //     loading: true,
-    //     indicator: <Audio width="50" />,
-    // });
     return (
         <>
             <h1 style={styles.h1Font}>BookList</h1>
@@ -64,56 +63,63 @@ const BookList = () => {
 
                 <div>
 
-                    {isLoading ? "Loading" : searchInput ?
-                        (title && title.filter(val => {
-                            if (searchInput === " ") {
+                    {isLoading ?
+                        <Box sx={{ width: '100%' }}>
+                            <LinearProgress color="success" />
+                        </Box>
+                        : searchInput ?
+                            (title && title.filter(val => {
+                                if (searchInput === " ") {
 
-                            } else if (val.title.toLowerCase().includes(searchInput.toLowerCase())) {
-                                console.log("val returns:", val)
-                                return val;
-                            }
-                        }).map((item, key) => {
-                            return (
-                                <div style={{ borderBottom: "lightgrey 1px solid" }} key={key}>
-                                    <Grid
-                                        container spacing={0.5}
-                                        onClick={() => {
-                                            navigate(`/bookdetails/${item.indexId}`)
-                                        }}
-                                    >
+                                } else if (val.title.toLowerCase().includes(searchInput.toLowerCase())) {
+                                    console.log("val returns:", val)
+                                    return val;
+                                }
+                            }).map((item, key) => {
+                                return (
+                                    <div style={styles.bookList} key={key}>
+                                        <Grid
+                                            container spacing={0.5}
+                                            onClick={() => {
+                                                navigate(`/bookdetails/${item.indexId}`)
+                                            }}
+                                        >
 
-                                        <Grid item sm={5} lg={4}>
-                                            <img alt="covers" style={styles.profileBookPics} src={item.imageURL} />
+                                            <Grid item sm={5} lg={4}>
+                                                <img alt="covers" style={styles.profileBookPics} src={item.imageURL} />
+                                            </Grid>
+                                            <Grid item sm={6} lg={4}>
+                                                <p style={styles.textBold}>{item.title}</p>
+                                                <p style={{ color: `${colours.baseDark}` }}><i>by {item.author}</i></p>
+                                            </Grid>
+
                                         </Grid>
-                                        <Grid item sm={6} lg={4}>
-                                            <p style={styles.textBold}>{item.title}</p>
-                                        </Grid>
+                                    </div>
+                                )
+                            })) : (title.slice(0, 5).map((item, key) => {
+                                return (
+                                    <div style={styles.bookList} key={key}>
+                                        <Grid
+                                            container spacing={0.5}
+                                            onClick={() => {
+                                                navigate(`/bookdetails/${item.indexId}`)
+                                            }}
+                                        >
 
-                                    </Grid>
-                                </div>
-                            )
-                        })) : (title.map((item, key) => {
-                            return (
-                                <div style={styles.bookList} key={key}>
-                                    <Grid
-                                        container spacing={0.5}
-                                        onClick={() => {
-                                            navigate(`/bookdetails/${item.indexId}`)
-                                        }}
-                                    >
+                                            <Grid item sm={5} lg={4}>
+                                                <img alt="covers" style={{ width: 150, height: 200 }} src={item.imageURL} />
+                                            </Grid>
+                                            <Grid item sm={6} lg={4}>
+                                                <p style={styles.textBold}>{item.title}</p>
+                                                <p style={{ color: `${colours.baseDark}` }}><i>by {item.author}</i></p>
+                                            </Grid>
 
-                                        <Grid item sm={5} lg={4}>
-                                            <img alt="covers" style={{ width: 150, height: 200 }} src={item.imageURL} />
                                         </Grid>
-                                        <Grid item sm={6} lg={4}>
-                                            <p style={styles.textBold}>{item.title}</p>
-                                        </Grid>
-
-                                    </Grid>
-                                </div>
-                            )
-                        }))
+                                    </div>
+                                )
+                            }))
                     }
+
                 </div>
 
             </div>
