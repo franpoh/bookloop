e0f0db8075a11000bd543116abef13ac1796f197
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-06 04:03:02 +0800

    replaced BrowserRouter with custom router for navigation outside of react context

--- web-app/src/screens/routing/index.js
+++ web-app/src/screens/routing/index.js
@@ -1,10 +1,7 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 
 import TopNav from "./top-nav";
-
-import styles from "../../styling/style-sheet";
-
 import Access from "../access";
 import Account from "../account";
 import Admin from "../admin";
@@ -12,9 +9,45 @@ import BookDetails from "../book-details";
 import BookList from "../book-list";
 import UploadBook from "../upload-book";
 
+import styles from "../../styling/style-sheet";
+import { history } from "../../components/history";
+
+// added custom router for navigating outside of react context
+// https://stackoverflow.com/questions/69871987/react-router-v6-navigate-outside-of-components
+
+// Router: The common low-level interface for all router components. 
+// The most common use-case for using the low-level <Router> is to synchronize a custom history with a state management lib like Redux or Mobx
+
+// First create a history object (found in components/history.js)
+// to be used in CustomRouter
+
+const CustomRouter = ({ history, ...props }) => {
+    const [state, setState] = React.useState({
+        action: history.action, // action - (string) The current action (PUSH, REPLACE, or POP)
+        location: history.location // location - (object) The current location. we are using pathname, path of the URL
+    });
+
+    // useLayoutEffect is identical to useEffect, but it fires synchronously after all DOM mutations. 
+    // Use this to read layout from the DOM and synchronously re-render.
+
+    // history.listen detects route change
+    // effect runs every time the history dependency changes
+    React.useLayoutEffect(() => history.listen(setState), [history]);
+
+    // Wrap with Router
+    return (
+        <Router
+            {...props}
+            location={state.location}
+            navigationType={state.action}
+            navigator={history}
+        />
+    );
+};
+
 function RoutePages() {
     return (
-        <BrowserRouter>
+        <CustomRouter history={history}>
             <div style={{ ...styles.container, ...styles.border }}>
                 <TopNav />
                 <div>
@@ -28,7 +61,7 @@ function RoutePages() {
                     </Routes>
                 </ div>
             </div>
-        </BrowserRouter>
+        </CustomRouter>
     )
 }
 
