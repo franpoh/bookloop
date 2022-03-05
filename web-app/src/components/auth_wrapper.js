9010f17cc31232774804079c7e55b76cb41aa81e
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-06 04:49:33 +0800

    cleaned up code

--- web-app/src/components/auth_wrapper.js
+++ web-app/src/components/auth_wrapper.js
@@ -5,16 +5,19 @@ const authWrapper = (apiCall) => {
     return apiCall.then((res) => {
         return res;
     }).catch((err) => {
-        callAlertOnce("You are not logged in! Redirecting...");
+        if (err.toString().indexOf("403") !== -1) {
+            callAlertOnce("You are not logged in! Redirecting...");
         return history.push("/access");
         // Push a new entry onto the history stack.
         // part of history library
         // https://github.com/remix-run/history/blob/3e9dab413f4eda8d6bce565388c5ddb7aeff9f7e/docs/navigation.md
+        } else {
+            console.log("AUTHWRAPPER", err);
+        }
     })
 }
 
 
 export default authWrapper;
 
-// Example: let profile = await authWrapper(bookAPI.get('/protected/viewprofile'));
 
