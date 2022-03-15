d9df962ee45f6299edc95118599a805107649854
CGW-G1 <91236669+CGW-G1@users.noreply.github.com>
2022-03-15 11:03:43 +0800

    Fixed display alignment on book details divider and added no image image

--- web-app/src/screens/book-details/components/show-book-info.js
+++ web-app/src/screens/book-details/components/show-book-info.js
@@ -4,6 +4,9 @@ import React from 'react';
 // styling
 import styles from "../../../styling/style-sheet.js";
 
+// assets
+const noImage = require("../../../assets/no-image.png");
+
 function ShowBookInfo ({ data }) {
 
     return (
@@ -11,7 +14,7 @@ function ShowBookInfo ({ data }) {
             <h1 style={styles.h1Font}>{data.title}</h1>
             <div style={styles.displayRow}>
                 <div style={styles.displayCard}>
-                    {(data.imageURL != null) ? <img src={data.imageURL} alt="Book Display" style={{ ...styles.profilePic, height: 'auto' }} /> : <div ></div>}
+                    <img src={data.imageURL ? data.imageURL : noImage} alt="Book Display" style={{ ...styles.profilePic, height: 'auto' }} />                    
                 </div>
                 <div style={{ marginLeft: 15 }}>
                     {<h1 style={{ ...styles.textBold, fontSize: '1em' }}>Author: {data.author}</h1>}
